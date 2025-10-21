90fd8d9845dfebc481004fa631e03f39afb33dd3
Jhon Jherick Maravilla <j.jaymaravilla@gmail.com>
2025-10-20 22:07:07 -0400

    Embed google forms in mix submission section

--- src/components/dark/home/mix-submission.jsx
+++ src/components/dark/home/mix-submission.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import DemoSubmission from '../demoSubmission/demo-submission.jsx';
 
 function MixSubmission() {
   return (
     <div className="sec-box portfolio section-padding bord-thin-bottom" data-scroll-index="4" >
-      <div className="sec-head mb-30">
+      <div className="sec-head mb-20">
         <div className="row">
           <div className="col-lg-8">
             <h6 className="sub-title opacity-7 mb-15">Demo & Mix Submission</h6>
@@ -14,21 +15,8 @@ function MixSubmission() {
           </div>
         </div>
 
-        <div className="sec-head mb-80 wow fadeInUp mt-100">
-          <div className="row justify-content-center">
-            <div className="col-lg-6 text-center">
-              <div className="butn-presv">
-                <a
-                  href="https://forms.gle/XfQXggtsdcCmjXB86"
-                  className="butn butn-md butn-bord radius-5 text-u full-width"
-                  target='blank'
-                >
-                  <img src="/assets/imgs/submit.png" alt="" />
-                  <h6>Submit your track here</h6>
-                </a>
-              </div>
-            </div>
-          </div>
+        <div className="sec-head wow fadeInUp">
+          <DemoSubmission />
         </div>
       </div>
     </div>
